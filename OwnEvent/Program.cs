023aa0220a0d1660d7f29699f04624fc95d9d20b
Стёпин Станислав Александрович <stanislavalex@mail.ru>
2021-03-05 14:03:42 +0300

    final

--- OwnEvent/Program.cs
+++ OwnEvent/Program.cs
@@ -10,6 +10,17 @@ namespace OwnEvent
     {
         static void Main(string[] args)
         {
+            string path = @"C:\TEST";
+
+            string text = "test";
+
+            StringFinder finder = new StringFinder();
+
+            finder.Start(path, text);
+            string result = finder.ParseStartResults(finder.Start(path, text));
+            finder.DisplayMessage(result);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ OwnEvent/StringFinder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwnEvent
+{
+    class StringFinder
+    {
+        public delegate void Found(string result);
+        public event Found Notify;
+
+        public (string, string) Start(string path, string text)
+        {
+            int lineCount = 0;
+            string fileName = "No";
+            var filePaths = Directory.GetFiles(path, "*.txt");
+            foreach (string file in filePaths)
+            {
+                lineCount = 0;
+                foreach (var line in File.ReadAllLines(file))
+                {
+                    lineCount += 1;
+                    if (line.Contains(text))
+                    {
+                        Notify += DisplayMessage;
+                        fileName = file;
+                    }
+                }
+            }
+            return (lineCount.ToString(), fileName);
+        }
+
+        public string ParseStartResults((string, string) start)
+        {
+            return $"{start.Item2} file contains this text in string {start.Item1}";
+        }
+
+        public void DisplayMessage(string result)
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
